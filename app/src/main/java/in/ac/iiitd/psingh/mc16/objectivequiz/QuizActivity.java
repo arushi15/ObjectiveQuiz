6dd9296b2c5b0b467b01c098a6c80ef784a93f2b
arushi15 <arushi14023@iiitd.ac.in>
2016-08-18 01:53:44 +0530

    First commit

---
+++ app/src/main/java/in/ac/iiitd/psingh/mc16/objectivequiz/QuizActivity.java
@@ -0,0 +1,142 @@
+package in.ac.iiitd.psingh.mc16.objectivequiz;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    int randnum;
+    private static final String TAG = "QuizActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+
+        mTrueButton = (Button) findViewById(R.id.TrueButton);
+        mFalseButton = (Button) findViewById(R.id.FalseButton);
+//        mTrueButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "Clicked True");
+//            }
+//        });
+         randnum=genrand1();
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+                if (primenum(randnum)==1)
+                {
+                    Toast.makeText(QuizActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(QuizActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (primenum(randnum)==1)
+                {
+                    Toast.makeText(QuizActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(QuizActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+    }
+
+    public int genrand1()
+    {
+        Random r1 = new Random();
+        final int randnum = r1.nextInt(1000) + 1;
+        TextView mytext = (TextView) findViewById(R.id.textViewer);
+        String mystring = String.valueOf(randnum);
+        String question = "Is " + mystring + " a prime number?";
+        mytext.setText(question);
+        return randnum;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "Inside onSaveInstance");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "Inside OnStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "Inside OnPause");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "Inside OnREsume");
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "Inside OnSTop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Inside OnDestroy");
+    }
+
+    int primenum(int n) {
+        int x = n / 2;
+        for (int i = 2; i <= x; i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public void genrandom(View view) {
+        genrand1();
+
+    }
+
+}
